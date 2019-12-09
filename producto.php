6df6c98bb795221e3b60406b6ae6586d96fb34aa
chaky <josemonzon99@gmail.com>
2019-12-09 20:53:05 -0300

    se agrego beta de pagina de producto, se arreglo el carrito y se mejoro el diseo

--- producto.php
+++ producto.php
@@ -8,11 +8,9 @@
   <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
-
+  <title>FAQ</title>
   <link rel="stylesheet" href="style.css">
-  <title>Carrito</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/css/bs-stepper.min.css">
+  <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 
 </head>
 
@@ -27,115 +25,60 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav mr-auto">
         <a class="nav-item nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="faq.html">FAQ</a>
+        <a class="nav-item nav-link active" href="faq.html">FAQ</a>
         <a class="nav-item nav-link" href="registro.html">Registro</a>
         <a class="nav-item nav-link" href="login.html">Login</a>
         <a class="nav-item nav-link" href="contacto.html">Contacto</a>
         <a class="nav-item nav-link" href="perfil.html">Perfil</a>
       </div>
       <form class="form-inline my-2 my-lg-0">
-        <a href="carrito.php"><button type="button" class="btn btn-outline-dark mr-sm-2">
-            <ion-icon id="cart" class="mt-1" name="cart"></ion-icon>
-          </button></a>
         <input id="buscarInput" class="input-group-text mr-sm-2 text-left" style="background-color: white" type="search" placeholder="Buscar productos..." aria-label="Search">
         <button id="buscarBoton" class="btn btn-outline-dark my-2 my-sm-0" type="submit">Buscar</button>
       </form>
     </div>
   </nav>
   <div class="border rounded container margenContacto">
-    <!-- stepper -->
-    <div class="bs-stepper">
-      <div class="bs-stepper-header" role="tablist">
-        <!-- your steps here -->
-        <div class="step" data-target="#logins-part">
-          <button type="button" class="step-trigger" role="tab" aria-controls="logins-part" id="logins-part-trigger">
-            <span class="bs-stepper-circle">1</span>
-            <span class="bs-stepper-label">Carrito</span>
-          </button>
-        </div>
-        <div class="line"></div>
-        <div class="step" data-target="#information-part">
-          <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
-            <span class="bs-stepper-circle">2</span>
-            <span class="bs-stepper-label">Informacion de entrega</span>
-          </button>
-        </div>
-        <div class="line"></div>
-        <div class="step" data-target="#information-part">
-          <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
-            <span class="bs-stepper-circle">3</span>
-            <span class="bs-stepper-label">Pago</span>
-          </button>
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <img class="card-img" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/vans.png" alt="Vans">
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row my-3">
+          <div class="col-12">
+            <h5>Vans Super Flama</h5>
+            <h6>Zapatillas Vans Super Flama</h6>
+            <h5>$2999.99</h5>
+          </div>
+          <div class="col-12 my-3">
+            <h6>Talles</h6>
+            <select class="custom-select mr-1">
+              <option selected>Seleccione un talle</option>
+              <option value="1">41</option>
+              <option value="2">42</option>
+              <option value="3">43</option>
+            </select>
+          </div>
+          <div class="col-12 my-3">
+            <h6>Color</h6>
+            <select class="custom-select mr-1">
+              <option selected>Seleccione un color</option>
+              <option value="1">Verdes</option>
+              <option value="2">Azules</option>
+              <option value="3">Negras</option>
+            </select>
+          </div>
+          <div class="col-12">
+            <button type="button" class="btn btn-outline-dark btn-block mt-3">Añadir al carrito</button>
+          </div>
+          <div class="col-12">
+            
+            <button type="button" class="btn btn-outline-dark btn-block mt-3"><ion-icon name="heart"></ion-icon> Añadir a favoritos</button>
+          </div>
         </div>
       </div>
-      <div class="bs-stepper-content">
-        <!-- your steps content here -->
-        <div id="logins-part" class="content" role="tabpanel" aria-labelledby="logins-part-trigger"></div>
-        <div id="information-part" class="content" role="tabpanel" aria-labelledby="information-part-trigger"></div>
-      </div>
+
     </div>
 
-  <div class="table-responsive">
-  <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Producto</th>
-          <th scope="col">Precio</th>
-          <th scope="col">Cantidad</th>
-          <th scope="col">Opciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">
-            <div class="row">
-              <div class="col-12 col-lg-6">
-                <img style="max-width:200px" class="img-thumbnail" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/vans.png" alt="" srcset="">
-              </div>
-              <div class="col-12 col-lg-4">
-                <h5>Zapatillas Vans</h5>
-                <p class="text-muted my-0">Color: Negro</p>
-                <p class="text-muted my-0">Talle: 42</p>
-              </div>
-            </div>
-          </th>
-          <td>99 USD</td>
-          <td>1</td>
-          <td>
-          <a href="editar.html">Editar</a>
-          <br>
-          <a href="fav.html">Añadir a favoritos</a>
-          <br>
-          <a href="borrar.html">Borrar</a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="row mb-3">
-      <div class="col-4 col-sm-4 col-lg-2">
-
-      </div>
-      <div class="col-6 col-sm-4 col-lg-2">
-        <div class="my-3">
-
-        </div>
-      </div>
-    </div>
   </div>
 
 
@@ -197,7 +140,6 @@
   </script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/bs-stepper/dist/js/bs-stepper.min.js"></script>
 </body>
 
 </html>
